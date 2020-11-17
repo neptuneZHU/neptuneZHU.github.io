92c32c9e2f687455e732fe7f02e01adf79ed09f1
neptuneZHU <yuting002@e.ntu.edu.sg>
2020-11-17 15:28:31 +0800

    add html

---
+++ www/untitled0.py
@@ -0,0 +1,188 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta name="generator" content="jemdoc, see http://jemdoc.jaboc.net/" />
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="jemdoc.css" type="text/css" />
+<title>Publications</title>
+</head>
+<body>
+<table summary="Table for page layout." id="tlayout">
+<tr valign="top">
+<td id="layout-menu">
+<div class="menu-category">Yuting Zhu</div>
+<div class="menu-item"><a href="index.html">Home</a></div>
+<div class="menu-item"><a href="bio.html">Biography</a></div>
+<div class="menu-item"><a href="contact.html">Contact</a></div>
+<div class="menu-category">Research Direction</div>
+<div class="menu-item"><a href="cyber.html">Cyber&nbsp;Security</a></div>
+<div class="menu-item"><a href="network.html">Networked&nbsp;Control</a></div>
+<div class="menu-category">Projects &amp; Publication</div>
+<div class="menu-item"><a href="projects.html">Projects</a></div>
+<div class="menu-item"><a href="publication.html">Publication</a></div>
+<div class="menu-category">Healthy Life Style</div>
+<div class="menu-item"><a href="travel.html">Travel</a></div>
+<div class="menu-item"><a href="book.html">Books</a></div>
+<div class="menu-item"><a href="music.html">Music</a></div>
+<div class="menu-item"><a href="cooperation.html">Co-authors</a></div>
+</td>
+<td id="layout-content">
+<div id="toptitle">
+<h1>Publications</h1>
+</div>
+<h2>Journals</h2>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+1.
+2.
+3.
+4.
+</pre></div></div>
+<h2>Conferences</h2>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+1. <b>Zhu, Y.</b>, Lin, L., Ware, S. and Su, R. <i>Supervisor synthesis for networked discrete event systems with communication delays and lossy channels.</i> In 2019 IEEE 58th Conference on Decision and Control (CDC) (pp. 6730-6735).
+2. <b>Zhu, Y.</b>, Lin, L. and Su, R. <i>Supervisor obfuscation against actuator enablement attack.</i> In 2019 18th European Control Conference (ECC) (pp. 1760-1765).
+3. Rashidinejad, A., Wetzels, B., Reniers, M., Lin, L., <b>Zhu, Y.</b>, &amp; Su, R. <i>Supervisory control of discrete-event systems under attacks: an overview and outlook.</i>  In 2019 18th European Control Conference (ECC) (pp. 1732-1739).
+4. Lin, L., Zhu, Y., &amp; Su, R. <i>Towards Bounded Synthesis of Resilient Supervisors.</i>  In 2019 IEEE 58th Conference on Decision and Control (CDC) (pp. 7659-7664).
+5. Ware, S., Yang, F., Zhu, Y., Su, R., &amp; Lin, L. (2018). <i>An efficient method of matrix multiplication for heaps of pieces.</i>  IFAC-PapersOnLine, 51(7), 206-211.
+6. Lin, L., Su, R., Brandin, B. A., Ware, S., Zhu, Y., &amp; Sun, Y. <i>Synchronous Composition of Finite Interval Automata.</i>  In 2019 IEEE 15th International Conference on Control and Automation (ICCA) (pp. 578-583).
+</pre></div></div>
+<p>You can specify a different output directory with <tt>-o</tt>:</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc -o html/ index
+</pre></div></div>
+<p>This will instead output to <tt>html/index.html</tt>.</p>
+<p>To change the html configuration (<a href="htmlchanges.html">details here</a>), use <tt>-c</tt>:</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc -c mysite.conf index
+</pre></div></div>
+<p>Command line options may be combined. For example, the following command will
+use <tt>mysite.conf</tt>, reading syntax from <tt>index.jemdoc</tt> and outputting to
+<tt>html/index.html</tt>:</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc -c mysite.conf -o html/ index
+</pre></div></div>
+<h2>eprints</h2>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc --version
+</pre></div></div>
+<p>To get simple command-line help, you can run <tt>jemdoc</tt> without arguments, or</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc --help
+</pre></div></div>
+<p>To show the html configuration (<a href="htmlchanges.html">details here</a>), run</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc --show-config
+</pre></div></div>
+<div id="footer">
+<div id="footer-text">
+Page generated 2020-11-17 09:36:06 Malay Peninsula Standard Time, by <a href="http://jemdoc.jaboc.net/">jemdoc</a>.
+</div>
+</div>
+</td>
+</tr>
+</table>
+</body>
+</html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta name="generator" content="jemdoc, see http://jemdoc.jaboc.net/" />
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="jemdoc.css" type="text/css" />
+<title>Publications</title>
+</head>
+<body>
+<table summary="Table for page layout." id="tlayout">
+<tr valign="top">
+<td id="layout-menu">
+<div class="menu-category">Yuting Zhu</div>
+<div class="menu-item"><a href="index.html">Home</a></div>
+<div class="menu-item"><a href="bio.html">Biography</a></div>
+<div class="menu-item"><a href="contact.html">Contact</a></div>
+<div class="menu-category">Research Direction</div>
+<div class="menu-item"><a href="cyber.html">Cyber&nbsp;Security</a></div>
+<div class="menu-item"><a href="network.html">Networked&nbsp;Control</a></div>
+<div class="menu-category">Projects &amp; Publication</div>
+<div class="menu-item"><a href="projects.html">Projects</a></div>
+<div class="menu-item"><a href="publication.html">Publication</a></div>
+<div class="menu-category">Healthy Life Style</div>
+<div class="menu-item"><a href="travel.html">Travel</a></div>
+<div class="menu-item"><a href="book.html">Books</a></div>
+<div class="menu-item"><a href="music.html">Music</a></div>
+<div class="menu-item"><a href="cooperation.html">Co-authors</a></div>
+</td>
+<td id="layout-content">
+<div id="toptitle">
+<h1>Publications</h1>
+</div>
+<h2>Journals</h2>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+1.
+2.
+3.
+4.
+</pre></div></div>
+<h2>Conferences</h2>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+1. <b>Zhu, Y.</b>, Lin, L., Ware, S. and Su, R. <i>Supervisor synthesis for networked discrete event systems with communication delays and lossy channels.</i> In 2019 IEEE 58th Conference on Decision and Control (CDC) (pp. 6730-6735).
+2. <b>Zhu, Y.</b>, Lin, L. and Su, R. <i>Supervisor obfuscation against actuator enablement attack.</i> In 2019 18th European Control Conference (ECC) (pp. 1760-1765).
+3. Rashidinejad, A., Wetzels, B., Reniers, M., Lin, L., <b>Zhu, Y.</b>, &amp; Su, R. <i>Supervisory control of discrete-event systems under attacks: an overview and outlook.</i>  In 2019 18th European Control Conference (ECC) (pp. 1732-1739).
+4. Lin, L., Zhu, Y., &amp; Su, R. <i>Towards Bounded Synthesis of Resilient Supervisors.</i>  In 2019 IEEE 58th Conference on Decision and Control (CDC) (pp. 7659-7664).
+5. Ware, S., Yang, F., Zhu, Y., Su, R., &amp; Lin, L. (2018). <i>An efficient method of matrix multiplication for heaps of pieces.</i>  IFAC-PapersOnLine, 51(7), 206-211.
+6. Lin, L., Su, R., Brandin, B. A., Ware, S., Zhu, Y., &amp; Sun, Y. <i>Synchronous Composition of Finite Interval Automata.</i>  In 2019 IEEE 15th International Conference on Control and Automation (ICCA) (pp. 578-583).
+</pre></div></div>
+<p>You can specify a different output directory with <tt>-o</tt>:</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc -o html/ index
+</pre></div></div>
+<p>This will instead output to <tt>html/index.html</tt>.</p>
+<p>To change the html configuration (<a href="htmlchanges.html">details here</a>), use <tt>-c</tt>:</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc -c mysite.conf index
+</pre></div></div>
+<p>Command line options may be combined. For example, the following command will
+use <tt>mysite.conf</tt>, reading syntax from <tt>index.jemdoc</tt> and outputting to
+<tt>html/index.html</tt>:</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc -c mysite.conf -o html/ index
+</pre></div></div>
+<h2>eprints</h2>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc --version
+</pre></div></div>
+<p>To get simple command-line help, you can run <tt>jemdoc</tt> without arguments, or</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc --help
+</pre></div></div>
+<p>To show the html configuration (<a href="htmlchanges.html">details here</a>), run</p>
+<div class="codeblock">
+<div class="blockcontent"><pre>
+jemdoc --show-config
+</pre></div></div>
+<div id="footer">
+<div id="footer-text">
+Page generated 2020-11-17 09:36:06 Malay Peninsula Standard Time, by <a href="http://jemdoc.jaboc.net/">jemdoc</a>.
+</div>
+</div>
+</td>
+</tr>
+</table>
+</body>
+</html>
